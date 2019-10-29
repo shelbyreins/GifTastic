008e1fbac9233f8231ce35012e3011ec6f2ab16b
Shelby Reinsimar <shelbyreins@yahoo.com>
2019-10-29 11:21:24 -0700

    create buttons for array

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,13 +4,19 @@ $(document).ready(function(){
 // Ajax call the specific object
 // Add the rating
 // Generate new buttons from the search engine
+//For loop to loop through 10 GIF's with ratings
 // Append the GIF and the rating once the button is pressed
 
 //https://api.giphy.com/v1/gifs/search?q=cats&limit=10&api_key=lWEJ1fgqWyR2Aj8W27Ojvzxt2i6OApp5
+
+// add variable for these and onclick
+//
+// use data attributes
+    // var still = gifDiv.dataset.still;
+    // var animate = gifDiv.dataset.animate;
+    // var state = gifDiv.dataset.state;
 // for still image used fixed_height
 // for animated image use fixed_height_still
-// add variable for these and onclick
-// use data attributes
 
    
 
@@ -24,26 +30,41 @@ $(document).ready(function(){
             url: queryURL,
             method: "GET"
         }).then(function(response){
-            var gifDiv = $("<div class='gif'>");
-            console.log(response)
-
-        })
-
-    }
-
-
-
-
+        var results = response.data;
+    
+        for (var i = 0; i < results.length; i++){
+            var gifDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating)
+            gifDiv.append(p);
+            
+            var gifImage = $("<img>");
 
 
+        }
 
+        });
+        }
 
+        function createButtons(){
+            $("#button-view").empty();
 
+            for(var i = 0; i < topics.length; i++){
+            var newButton = $("<button>");
+            newButton.addClass("btn");
+            newButton.attr("data-object", topics[i])
+            newButton.text(topics[i]);
+            $("#button-view").append(newButton);
+            }
+        }
 
+      
 
-
-
-
+    
+    $(document).on("click", ".btn", displayGif);
+    //$("#gif-view").prepend(gifDiv); 
+    
+    createButtons();
 });
+
 
 
